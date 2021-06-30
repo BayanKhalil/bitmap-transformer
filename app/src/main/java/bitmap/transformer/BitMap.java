08da8d00ba8093bbcd9342cd0c388b94e7b8e5f3
Bayan Khalil <bayan.khalil92@yahoo.com>
2021-06-30 16:14:01 +0300

    first trile

---
+++ app/src/main/java/bitmap/transformer/BitMap.java
@@ -0,0 +1,14 @@
+package bitmap.transformer;
+
+
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+
+public class BitMap {
+
+
+}
