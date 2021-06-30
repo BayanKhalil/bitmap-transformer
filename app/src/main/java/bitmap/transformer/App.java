f574c5e75abe8f935417bbb2c02185d2f24c185e
MKAbuMattar <mohammad.khaled@outlook.com>
2021-06-30 18:36:17 +0300

    add save image & class for BitMap

--- app/src/main/java/bitmap/transformer/App.java
+++ app/src/main/java/bitmap/transformer/App.java
@@ -8,56 +8,41 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.Callable;
-import javax.imageio.ImageIO;
 
 
 public class App {
 
 
-    public static void main(String[] args) {
-//          BitMap bitmapCatch= new BitMap();
-        String path = "D:\\401\\bitmap-transformer\\app\\src\\main\\resources\\download.png";
-        BufferedImage image = null;
-        int height = 0;
-        int width = 0;
+  public static void main(String[] args) {
 
-        try {
-            image = ImageIO.read(new File(path));
-            height = image.getHeight();
-            width = image.getWidth();
+    String path = "/home/mkabumattar/amman-code-java-401d2/bitmap-transformer/app/src/main/resources/download.png";
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println(height);
+    BitMap newImage = new BitMap(path);
+//    String outpot = args[1];
 
-        convertBlackAndWhite(image,height,width);
-    }
+    newImage.invert();
+    String imagePath = "bitmap/";
+    String imageName = "name.jpg";
+    newImage.saveImage((imagePath + imageName));
 
-
-    public static void convertBlackAndWhite(BufferedImage image, int height, int width) {
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                Color blackAndWhite = new Color(image.getRGB(i, j));
-
-                int red = blackAndWhite.getRed();
-                int blue = blackAndWhite.getBlue();
-                int green = blackAndWhite.getGreen();
-                int colom = (red + blue + green) / 3;
-                if (colom > 128) {
-                    colom = -1;
-                } else {
-                    colom = -2056;
-                }
-                image.setRGB(i, j, colom);
+//    switch (args[2]) {
+//      case "1":
+//        newImage.convertBlackAndWhite();
+//        newImage.saveImage(outpot);
+//        break;
+//      case "2":
+//        newImage.invert();
+//        newImage.saveImage(outpot);
+//        break;
+//      default:
+//        break;
+//    }
 
 
-            }
-        }
+  }
 
-    }
+
 }
